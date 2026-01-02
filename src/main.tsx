0ef38c5b12513f571f6adf1a35f0bf0aea5bc65c
ahuglajbclajep <ahuglajbclajep@gmail.com>
2026-01-02 19:37:54 +0900

    とりあえずざっくり動きを作る

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,71 @@
+import { useState, useCallback } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <h1 className="bg-black text-white">Hello, React!</h1>,
-);
+import { Input } from "./Input";
+import { Template, templates, toString } from "./template";
+
+const App = () => {
+  const [template, setTemplate] = useState<Template>({
+    context: [],
+    subject: [],
+    place: [],
+  });
+
+  const onChangeTemplate = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) =>
+      void setTemplate(templates[e.target.value]),
+    [],
+  );
+
+  const onUpdateTemplate = useCallback(
+    (type: "context" | "subject" | "place", index: number) =>
+      (value: string) => {
+        setTemplate((prev) => {
+          const newTemplate = { ...prev };
+          newTemplate[type][index] = value;
+          return newTemplate;
+        });
+      },
+    [],
+  );
+
+  return (
+    <div>
+      <select onChange={onChangeTemplate}>
+        {Object.keys(templates).map((template) => (
+          <option key={template} value={template}>
+            {template}
+          </option>
+        ))}
+      </select>
+      <h1>場面</h1>
+      <ul>
+        {template.context.map((context, i) => (
+          <li key={context}>
+            <Input value={context} onChange={onUpdateTemplate("context", i)} />
+          </li>
+        ))}
+      </ul>
+      <h1>写っているもの・話題の対象</h1>
+      <ul>
+        {template.subject.map((subject, i) => (
+          <li key={subject}>
+            <Input value={subject} onChange={onUpdateTemplate("subject", i)} />
+          </li>
+        ))}
+      </ul>
+      <h1>場所・地域</h1>
+      <ul>
+        {template.place.map((place, i) => (
+          <li key={place}>
+            <Input value={place} onChange={onUpdateTemplate("place", i)} />
+          </li>
+        ))}
+      </ul>
+      {toString(template)}
+    </div>
+  );
+};
+
+createRoot(document.getElementById("root")!).render(<App />);
---
+++ src/template.ts
@@ -0,0 +1,33 @@
+export type Template = {
+  context: string[];
+  subject: string[];
+  place: string[];
+};
+
+export const templates: Record<string, Template> = {
+  散歩: {
+    context: ["散歩", "街歩き", "東京散歩"],
+    subject: ["季節の花"],
+    place: ["神代植物公園", "調布", "東京"],
+  },
+  旅行: {
+    context: ["旅行", "街歩き", "初詣"],
+    subject: ["スイーツ"],
+    place: ["伊勢神宮おかげ横丁", "伊勢神宮", "伊勢", "三重"],
+  },
+  美術館: {
+    context: ["美術館", "企画展"],
+    subject: ["国宝", "曜変天目茶碗", "曜変天目"],
+    place: ["静嘉堂文庫美術館", "丸の内", "東京"],
+  },
+  キャンプ: {
+    context: ["キャンプ"],
+    subject: ["キャンプ場", "キャンプ飯", "焚き火"],
+    place: ["長瀞", "秩父", "埼玉"],
+  },
+};
+
+export const toString = (edited: Template) =>
+  [...edited.context, ...edited.subject, ...edited.place]
+    .map((tag) => `#${tag}`)
+    .join(" ");
