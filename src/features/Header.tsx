0b0773d872925f975e4bdf982f249dc6948399f8
ahuglajbclajep <ahuglajbclajep@gmail.com>
2026-01-04 16:20:18 +0900

    アプリの名前も更新

--- src/features/Header.tsx
+++ src/features/Header.tsx
@@ -2,7 +2,7 @@ export const Header = () => {
   return (
     <header className="flex flex-col items-center gap-y-2">
       <h1 className="bg-linear-to-tr from-yellow-400 via-red-500 to-purple-600 bg-clip-text text-2xl font-bold text-transparent">
-        SNS Tag Builder
+        Hashtag Builder
       </h1>
       <p className="text-xs font-medium text-gray-400 italic">
         よく使うハッシュタグをメモできるやつ
